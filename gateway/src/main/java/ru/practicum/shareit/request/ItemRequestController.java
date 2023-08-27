4e16e1fe442022ae1f81bf44b41ddf1d9531829a
Evgenya Ogoreltseva <zhenyakirov43555@gmail.com>
2023-08-27 22:55:24 +0300

    Добавила логирование и удалила закомментированный код

--- gateway/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ gateway/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.request;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
@@ -14,6 +15,7 @@ import javax.validation.constraints.PositiveOrZero;
 @RequiredArgsConstructor
 @RequestMapping("/requests")
 @Validated
+@Slf4j
 public class ItemRequestController {
 
     private final ItemRequestClient itemRequestClient;
@@ -21,11 +23,13 @@ public class ItemRequestController {
     @PostMapping
     ResponseEntity<Object> createItemRequest(@RequestHeader("X-Sharer-User-Id") Long userId,
                                            @Valid @RequestBody ItemRequestDto itemRequestDto) {
+        log.info("Item request created by user with id " + userId);
         return itemRequestClient.createItemRequest(userId, itemRequestDto);
     }
 
     @GetMapping
     ResponseEntity<Object> getAllItemRequestByUserId(@RequestHeader("X-Sharer-User-Id") Long userId) {
+        log.info("Got list of item requests made by user with id " + userId);
         return itemRequestClient.getAllItemRequestByUserId(userId);
     }
 
@@ -33,11 +37,13 @@ public class ItemRequestController {
     ResponseEntity<Object> getAllItemRequests(@RequestHeader("X-Sharer-User-Id") Long userId,
                                           @PositiveOrZero @RequestParam(name = "from", defaultValue = "0") Integer from,
                                           @Positive @RequestParam(name = "size", defaultValue = "10") Integer size) {
+        log.info("Got List of item requests for user with id " + userId + "from " + from + "to " +size);
         return itemRequestClient.getAllItemRequests(userId, from, size);
     }
 
     @GetMapping("/{requestId}")
     ResponseEntity<Object> getItemRequestById(@RequestHeader("X-Sharer-User-Id") Long userId, @PathVariable Long requestId) {
+        log.info("Got Item request by id " + requestId);
         return itemRequestClient.getItemRequestById(requestId, userId);
     }
 }
